0a40a14d3ce6d38ff773ee708d9d8d338c25dd5a
pupubeat <ratenas95@gmail.com>
2024-08-25 22:29:26 -0400

    agregar boton de carga, configuracion de ambos btn en app.js

--- Bootstrap 5 - Practica 3/app.js
+++ Bootstrap 5 - Practica 3/app.js
@@ -1,4 +1,8 @@
+// Capturar datos del HTML
 const formulario = document.querySelector('#formulario')
+const btnEnviar = document.querySelector('#botonEnviar')
+const btnCargando = document.querySelector('#botonCargando')
+
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
     // Obtener datos de los label del formulario.
@@ -7,4 +11,17 @@ formulario.addEventListener('submit', (e) => {
     console.log('Correo Electrónico:', datos.get('emailLabel'))
     // Mostrar en consola el dato recibido en label Password
     console.log('Password:', datos.get('passwordLabel'))
+
+    // Agregar-remover el class d-none en ambos botones del formulario.
+    btnEnviar.classList.add('d-none')
+    btnCargando.classList.remove('d-none')
+
+    // Después de 3 segundos, revertir los cambios en los botones.
+    window.setTimeout(() => {
+        btnCargando.classList.add('d-none')
+        btnEnviar.classList.remove('d-none')
+    }, 3000)
+
+    // Limpiar los datos del formulario.
+    formulario.reset()
 })
