e2363d89fc70e0f82d925d3a3511c40219516f22
pupubeat <ratenas95@gmail.com>
2024-08-25 22:06:23 -0400

    app.js: obtencion data de los labels email y password

--- Bootstrap 5 - Practica 3/app.js
+++ Bootstrap 5 - Practica 3/app.js
@@ -1,5 +1,10 @@
 const formulario = document.querySelector('#formulario')
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
+    // Obtener datos de los label del formulario.
     const datos = new FormData(formulario)
+    // Mostrar en consola el dato recibido en label Email
+    console.log('Correo Electrónico:', datos.get('emailLabel'))
+    // Mostrar en consola el dato recibido en label Password
+    console.log('Password:', datos.get('passwordLabel'))
 })
