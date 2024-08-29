74b96c0743f8f7290d89dcc10cb4dabe8ac6983b
pupubeat <ratenas95@gmail.com>
2024-08-28 22:42:01 -0400

    aplicar JS al numero de visitas

--- Bootstrap 5 - Practica 5/js/app.js
+++ Bootstrap 5 - Practica 5/js/app.js
@@ -1,8 +1,10 @@
 const rango = document.getElementById('customRange3')
 const visitas = document.getElementById('visitas')
 const precio = document.getElementById('precio')
+const arrayVisitas = ['10K', '50k', '100k', '500k', '1M']
 
 rango.addEventListener('input', () => {
     console.log(rango.value)
     precio.textContent = rango.value
+    visitas.textContent = arrayVisitas[((rango.value / 8) - 1)]
 })
