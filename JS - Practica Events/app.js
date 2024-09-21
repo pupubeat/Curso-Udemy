1d8e2ffbaa7554da8006c3fb87b27bcddc11f90d
pupubeat <ratenas95@gmail.com>
2024-09-20 21:47:54 -0300

    app.js: obtener elementos del html

---
+++ JS - Practica Events/app.js
@@ -0,0 +1,5 @@
+// Obtener elementos desde el HTML
+const colorInput = document.getElementById(colorInput)
+const btnVisualizar = document.getElementById(btnVisualizar)
+const twtExample = document.getElementById(twtExample)
+const cardColor = document.getElementById(cardColor)
