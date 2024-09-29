2dc5fe1d09515e53398fc54b92479f27da9c5d9f
pupubeat <ratenas95@gmail.com>
2024-09-29 19:43:00 -0300

    app.js: obtener valores desde html

---
+++ JS - Formulario y expresiones regulares/app.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById('formulario')
+const userName = document.getElementById('userName')
+const userEmail = document.getElementById('userEmail')
+
+formulario.addEventListener("submit", e => {
+    e.preventDefault()
+})
