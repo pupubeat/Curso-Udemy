717b749a218100f7e4eaac99411fd4090ef6a845
pupubeat <ratenas95@gmail.com>
2024-09-29 20:24:35 -0300

    html: creadas alertas para contenido dinamico js

--- JS - Formulario y expresiones regulares/app.js
+++ JS - Formulario y expresiones regulares/app.js
@@ -2,6 +2,22 @@ const formulario = document.getElementById('formulario')
 const userName = document.getElementById('userName')
 const userEmail = document.getElementById('userEmail')
 
+// Expresiones ReGex
+const regUsername = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+
+// Lógica formulario.
 formulario.addEventListener("submit", e => {
     e.preventDefault()
+
+    // Validaciones
+    if (!regUsername.test(userName.value)) {
+        console.log('Formato no válido.')
+        return
+    }
+
+    if (!regEmail.test(userEmail.value)) {
+        console.log('Formato no válido.')
+        return
+    }
 })
