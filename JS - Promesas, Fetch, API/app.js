1ad5de3bcd49a0c84580d9e404ac336a653fbb67
pupubeat <ratenas95@gmail.com>
2024-10-04 17:26:55 -0300

    app.js: domcontentloaded

--- JS - Promesas, Fetch, API/app.js
+++ JS - Promesas, Fetch, API/app.js
@@ -1,3 +1,18 @@
 // Obtener datos desde el HTML
 const cards = document.getElementById('card-dinamicas')
 const templateCards = document.getElementById('template-card').content
+
+// Esperar a que se cargue por completo el DOM antes de proceder a funciones asincronicas.
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+
+    } catch (error) {
+
+    } finally {
+
+    }
+}
