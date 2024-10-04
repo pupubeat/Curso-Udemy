cb31e4f58c772abbdd1d1b7e046a0619f8e1820a
pupubeat <ratenas95@gmail.com>
2024-10-04 19:05:29 -0300

    app.js: fetch data listo

--- JS - Promesas, Fetch, API/app.js
+++ JS - Promesas, Fetch, API/app.js
@@ -1,6 +1,7 @@
 // Obtener datos desde el HTML
-const cards = document.getElementById('card-dinamicas')
-const templateCards = document.getElementById('template-card').content
+const cards = document.getElementById('card-dinamicas');
+const templateCards = document.getElementById('template-card').content;
+const loading = document.getElementById('loading');
 
 // Esperar a que se cargue por completo el DOM antes de proceder a funciones asincronicas.
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,10 +10,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const fetchData = async () => {
     try {
+        loadingData(true);
+        const res = await fetch("https://rickandmortyapi.com/api/character");
+        const data = await res.json();
+        mostrarData(data);
 
     } catch (error) {
-
+        console.log(error);
     } finally {
-
+        loadingData(false);
     }
+};
+
+const mostrarData = async (data) => {
+    console.log(data)
 }
+
+const loadingData = estado => {
+    if (estado) {
+        loading.classList.remove('d-none');
+    } else {
+        loading.classList.add('d-none');
+    }
+};
