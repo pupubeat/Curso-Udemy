11ac0eeb110c1a074e64e6415e0fd4f3590bdc93
pupubeat <ratenas95@gmail.com>
2024-10-06 00:25:20 -0300

    app.js: capturar datos desde html

---
+++ JS - Practica POO/app.js
@@ -0,0 +1,6 @@
+// Capturar elementos desde el HTML
+const formulario = document.getElementById('formulario')
+const cardEstudiantes = document.getElementById('cardEstudiantes')
+const cardProfesor = document.getElementById('cardProfesor')
+const templateEstudiante = document.getElementById('templateEstudiante').content
+const templateProfesor = document.getElementById('templateProfesor').content
