1baed61774114161962e05fdba99e40dcc7e240f
pupubeat <ratenas95@gmail.com>
2024-10-07 23:02:35 -0300

    creacion class persona

--- JS - Practica POO/app.js
+++ JS - Practica POO/app.js
@@ -4,3 +4,11 @@ const cardEstudiantes = document.getElementById('cardEstudiantes')
 const cardProfesor = document.getElementById('cardProfesor')
 const templateEstudiante = document.getElementById('templateEstudiante').content
 const templateProfesor = document.getElementById('templateProfesor').content
+
+// Creación de objetos: Persona, Estudiante y Profesor.
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad
+    }
+}
