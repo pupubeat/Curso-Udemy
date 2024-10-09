750d0523b104dcaae30e37d7d99aa8abfbf684d4
pupubeat <ratenas95@gmail.com>
2024-10-08 21:28:57 -0300

    arreglos en logica del form + clases

--- JS - Practica POO/app.js
+++ JS - Practica POO/app.js
@@ -1,11 +1,12 @@
 // Capturar elementos desde el HTML
-const formulario = document.getElementById('formulario')
-const cardEstudiantes = document.getElementById('cardEstudiantes')
-const cardProfesor = document.getElementById('cardProfesor')
-const templateEstudiante = document.getElementById('templateEstudiante').content
-const templateProfesor = document.getElementById('templateProfesor').content
+const formulario = document.getElementById('formulario');
+const cardEstudiantes = document.getElementById('cardEstudiantes');
+const cardProfesor = document.getElementById('cardProfesor');
+const templateEstudiante = document.getElementById('templateEstudiante').content;
+const templateProfesor = document.getElementById('templateProfesor').content;
 
 const estudiantes = []; // Array para agrupar todos los estudiantes a agregar en el formulario
+const profesores = [];
 
 // Creación de objetos: Persona, Estudiante y Profesor.
 
@@ -17,12 +18,20 @@ class Persona {
     }
     static mostrarPersonaUI(personas, tipo) {
         if (tipo === 'Estudiante') {
-            cardEstudiantes.textContent = ''
-            const fragment = document.createDocumentFragment()
+            cardEstudiantes.textContent = '';
+            const fragment = document.createDocumentFragment();
             personas.forEach(item => {
-                fragment.appendChild(item.agregarNuevoEstudiante())
+                fragment.appendChild(item.agregarNuevoEstudiante());
             })
-            cardEstudiantes.appendChild(fragment)
+            cardEstudiantes.appendChild(fragment);
+        }
+        if (tipo === 'Profesor') {
+            cardProfesor.textContent = '';
+            const fragment = document.createDocumentFragment();
+            personas.forEach(item => {
+                fragment.appendChild(item.agregarNuevoProfesor());
+            })
+            cardProfesor.appendChild(fragment);
         }
     }
 }
@@ -36,15 +45,15 @@ class Estudiante extends Persona {
         this.#estado = estado
     }
 
-    get getEstado() {
+    get getEstudiante() {
         return this.#estudiante
     }
 
     agregarNuevoEstudiante() {
-        const clone = templateEstudiante.cloneNode(true)
-        clone.querySelector('h5').textContent = this.nombre
-        clone.querySelector('h6').textContent = this.#estudiante
-        clone.querySelector('.lead').textContent = this.edad
+        const clone = templateEstudiante.cloneNode(true);
+        clone.querySelector('h5').textContent = this.nombre;
+        clone.querySelector('h6').textContent = this.getEstudiante;
+        clone.querySelector('.lead').textContent = this.edad + ' años';
         return clone
     }
 }
@@ -54,11 +63,11 @@ class Profesor extends Persona {
     #profesor = "Profesor"
 
     agregarNuevoProfesor() {
-        const clone = templateProfesor.cloneNode(true)
-        clone.querySelector('h5').textContent = this.nombre
-        clone.querySelector('h6').textContent = this.#profesor
-        clone.querySelector('.lead').textContent = this.edad
-        return clone
+        const clone = templateProfesor.cloneNode(true);
+        clone.querySelector('h5').textContent = this.nombre;
+        clone.querySelector('h6').textContent = this.#profesor;
+        clone.querySelector('.lead').textContent = this.edad + ' años';
+        return clone;
     }
 }
 
@@ -68,13 +77,16 @@ formulario.addEventListener('submit', (e) => {
     // console.log([...datos.values()])
     const [nombre, edad, opcion] = [...datos.values()]; // desestructurar los values de la data del formulario.
 
-    // Condición si elijes la opción de estudiante
+    // Condición si elijes la opción de estudiante.
     if (opcion === 'Estudiante') {
         const estudiante = new Estudiante(nombre, edad);
-        estudiantes.push(estudiante) // Agregar nuevo estudiante al array vacío.
+        estudiantes.push(estudiante); // Agregar nuevo estudiante al array vacío.
+        Persona.mostrarPersonaUI(estudiantes, opcion);
     }
-
+    // Condición si elijes la opción de profesor.
     if (opcion === 'Profesor') {
-
+        const profesor = new Profesor(nombre, edad);
+        profesores.push(profesor);
+        Persona.mostrarPersonaUI(profesores, opcion);
     }
 })
