493acccb4a82288381d536fa3d1554c9a76dc4f1
pupubeat <ratenas95@gmail.com>
2024-10-15 22:50:28 -0300

    formulario addEventListener submit

---
+++ LocalStorage/app.js
@@ -0,0 +1,22 @@
+const formulario = document.getElementById('formulario')
+const alertInput = document.getElementById('alertInput')
+const mostrarToDo = document.getElementById('mostrarToDo')
+const templateToDO = document.getElementById('templateToDO').content
+
+let todos = []
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+    alertInput.classList.add('d-none')
+
+    const data = new FormData(formulario);
+    const [todo] = [...data.values()];
+
+    if (!todo.trim()) {
+        alertInput.classList.remove('d-none')
+        return
+    }
+
+    agregarToDo(todo);
+    pintarToDos();
+})
