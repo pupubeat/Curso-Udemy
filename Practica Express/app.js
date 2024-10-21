1f2acc11656545b0bed4c4451cb743424ae4cfb0
pupubeat <ratenas95@gmail.com>
2024-10-21 19:54:34 -0300

    plantillas hbs + renders home/index

--- Practica Express/app.js
+++ Practica Express/app.js
@@ -1,13 +1,32 @@
 import express from 'express'
+import 'dotenv/config'
+import { engine, create } from 'express-handlebars';
 
 const app = express();
+const hbs = create({
+    extname: '.hbs',
+    partialsDir: ['views/partials']
+});
 
-// Archivos estáticos públicos.
-app.use(express.static('public'));
+// Configurar Handlebars.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
 // Middlewares para activar el req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Archivos estáticos públicos.
+app.use(express.static('public'));
 
 // Conectar al puerto 3000 o a uno en específico.
 const PORT = process.env.PORT
