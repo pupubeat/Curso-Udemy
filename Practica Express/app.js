e7598d81f1b6cf0d5acb260606e888c1159f659a
pupubeat <ratenas95@gmail.com>
2024-10-25 00:06:13 -0300

    database + data.js

--- Practica Express/app.js
+++ Practica Express/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import 'dotenv/config'
 import { engine, create } from 'express-handlebars';
+import loginRoute from './routes/auth.route.js'
+import homeRoute from './routes/home.route.js'
 
 const app = express();
 const hbs = create({
@@ -13,20 +15,16 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-
 // Middlewares para activar el req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Archivos estáticos públicos.
 app.use(express.static('public'));
+
+// Rutas.
+app.use('/', homeRoute)
+app.use('/login', loginRoute)
 
 // Conectar al puerto 3000 o a uno en específico.
 const PORT = process.env.PORT
