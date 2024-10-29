b230107f25dfa4a5c00a2b56a65f495559123aa8
pupubeat <ratenas95@gmail.com>
2024-10-28 23:50:24 -0300

    url.js: mongoose + nanoid

---
+++ Practica Express/models/Url.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid'
+const { Schema } = mongoose;
+
+const urlSchema = new Schema({
+    origin: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    shortURL: {
+        type: String,
+        unique: true,
+        required: true,
+        default: nanoid()
+    }
+})
