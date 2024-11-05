af4b86a3cfff17c4ee267ee7f4b501a167ad9e00
pupubeat <ratenas95@gmail.com>
2024-11-04 23:31:19 -0300

    home route POST

--- Practica Express/routes/home.route.js
+++ Practica Express/routes/home.route.js
@@ -3,6 +3,10 @@ import { homeControllers } from "../controllers/homeControllers.js";
 
 const router = Router()
 
+// Obtener listado urls
 router.get('/', homeControllers.leerUrls)
+
+// Agregar una nueva url
+router.post('/', homeControllers)
 
 export default router
